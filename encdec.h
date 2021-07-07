eee7b082dfb1652b920f7d800d7ee33d5a2107cd
Nihal Jere <nihal@nihaljere.xyz>
2021-07-07 14:34:13 -0500

    encdec: export xwrite

--- encdec.h
+++ encdec.h
@@ -5,3 +5,4 @@ int atd_cmd_call_events(int fd);
 int atd_status_call(int fd, enum callstatus status, char *num);
 ssize_t dec_str(char *in, char **out);
 int dec_call_status(int fd, struct call *calls);
+int xwrite(int fd, char *buf, size_t len);
